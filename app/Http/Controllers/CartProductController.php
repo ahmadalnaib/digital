02c29d2ab3ca4f1a6f742f11bfaf048ed3d6cde2
ahmedhabeeb73 <alnaib888@gmail.com>
2021-10-06 12:42:20 +0200

    adding products to a cart

--- app/Http/Controllers/CartProductController.php
+++ app/Http/Controllers/CartProductController.php
@@ -8,15 +8,19 @@ use Illuminate\Http\Request;
 
 class CartProductController extends Controller
 {
-    
+
     public function store(Request $request)
     {
-        $product=Product::findOrFail($request->product_id);
+        $product = Product::findOrFail($request->product_id);
 
-        $cart=Cart::firstOrCreate([
-            'user_id'=>auth()->id(),
-            'session_id'=>session()->getId(),
-            
+        $cart = Cart::firstOrCreate([
+            'user_id' => auth()->id(),
+            'session_id' => session()->getId(),
+
         ]);
+
+        $cart->products()->syncWithoutDetaching($product);
+
+        return back();
     }
 }
